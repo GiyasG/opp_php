0de6b5b4262600da53960cf7b4f67ca312a32f4e
User1 <C:\Users\Embassy\AppData\Roaming\The Bat!>
2018-11-29 18:17:16 +0300

    added dbase upload of a new record

--- php/upload.php
+++ php/upload.php
@@ -1,6 +1,9 @@
 <?php
 // print_r($_POST['item']);
-
+if (isset($_POST['item'])) {
+  $items = $_POST['item'];
+  // print_r($items);
+}
 if(isset($_FILES['file'])){
     $errors= array();
     $file_name = $_FILES['file']['name'];
@@ -20,7 +23,9 @@ if(isset($_FILES['file'])){
     }
     if(empty($errors)==true){
         $currentDate = date('ymdhms');
-        move_uploaded_file($file_tmp,"../img/".$currentDate.$file_name);
+        $newFileName = $currentDate.$file_name;
+        move_uploaded_file($file_tmp,"../img/".$newFileName);
+        toDbase($items, $newFileName);
         echo 'File uploaded successfully';
     }else{
         print_r($errors[0]);
@@ -31,4 +36,43 @@ else{
     $errors[]="No image found";
     print_r($errors[0]);
 }
+
+function toDbase($items, $newFileName) {
+  include('class/mysql_crud.php');
+  $db = new Database();
+  $db->connect();
+  $db->setName('SET NAMES \'utf8\'');
+
+  $db->insert('stock',array('sname'=>$items['sname'],
+            'sdescription'=>$items['sdescription'],
+            'simage'=>$newFileName, 'sprice'=>$items['sprice'],
+            's39_all'=>$items['sizes']['i39']['all'], 's39_reserved'=>'0', 's39_forsale'=>'0', 's39_sold'=>'0',
+            's40_all'=>$items['sizes']['i40']['all'], 's40_reserved'=>'0', 's40_forsale'=>'0', 's40_sold'=>'0',
+            's41_all'=>$items['sizes']['i41']['all'], 's41_reserved'=>'0', 's41_forsale'=>'0', 's41_sold'=>'0',
+            's42_all'=>$items['sizes']['i42']['all'], 's42_reserved'=>'0', 's42_forsale'=>'0', 's42_sold'=>'0',
+            's43_all'=>$items['sizes']['i43']['all'], 's43_reserved'=>'0', 's43_forsale'=>'0', 's43_sold'=>'0',
+            's44_all'=>$items['sizes']['i44']['all'], 's44_reserved'=>'0', 's44_forsale'=>'0', 's44_sold'=>'0',
+            's45_all'=>$items['sizes']['i45']['all'], 's45_reserved'=>'0', 's45_forsale'=>'0', 's45_sold'=>0));
+  // Table name, column names and respective values
+  // echo $db->getSql();
+  $res = $db->getResult();
+  // print_r($res);
+  if (!$res)
+  {
+  die('Cant connect: ' . mysql_error());
+  }
+  else {
+    // $my_file = '../img/'.$newFileName;
+    // $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file); //implicitly creates file
+    //     if ($handle) {
+    //     file_put_contents($my_file, file_get_contents($_FILES["userImage"]["tmp_name"]));
+    //     echo 'Uploaded';
+    //    } else {
+        echo 'Uploaded';
+       // }
+      }
+$db->disconnect();
+}
+
+
 ?>
