df394a1f85a35590d6887ac6a722e32d89c26151
User1 <C:\Users\Embassy\AppData\Roaming\The Bat!>
2018-12-06 17:34:48 +0300

    changes to server side access to dbase and in admin html component

--- php/update.php
+++ php/update.php
@@ -1,54 +1,74 @@
 <?php
-// print_r($_POST['item']);
-if (isset($_POST['item'])) {
-  $items = $_POST['item'];
-  // print_r($items);
-  // print_r($_FILES['file']);
-}
-if(isset($_FILES['file'])){
-    $errors= array();
-    $file_name = $_FILES['file']['name'];
-    $file_size =$_FILES['file']['size'];
-    $file_tmp =$_FILES['file']['tmp_name'];
-    $file_type=$_FILES['file']['type'];
-    $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
-    $extensions = array("jpeg","jpg","png","pdf");
-    if(in_array($file_ext,$extensions )=== false){
-         $errors[]="File extension not allowed, please choose a JPEG or PNG file.";
-    }
-    if($file_size > 1097152){
-        $errors[]='File size cannot exceed 1 MB';
-    }
-    if(file_exists('../img/'.$file_name)) {
-      $errors[]='File '.$file_name.' already exists';
-    }
-    if(empty($errors)==true){
-        $currentDate = date('ymdhms');
-        $newFileName = $currentDate.$file_name;
-        move_uploaded_file($file_tmp,"../img/".$newFileName);
-        $rid = toDbase($items, $newFileName);
-
-        $items['image'] = '../img/'.$newFileName;
-        $outp2 = '{"message": "The record and the image updated successfully"}';
-        $outp1 = '{"updateitem":['.json_encode($items).']}';
-        $outp  = '{"info":['.$outp1.','.$outp2.']}';
-
-        echo ($outp);
-    }else{
-        print_r($errors[0]);
-    }
-}
-else{
-  $newFileName = $items['image'];
-  toDbase($items, $newFileName);
-
-  $outp2 = '{"message": "The record updated with the current image successfully"}';
-  $outp1 = '{"updateitem":['.json_encode($items).']}';
-  $outp  = '{"info":['.$outp1.','.$outp2.']}';
-
-  echo ($outp);
+require '../vendor/autoload.php';
+  $db1 = new \PDO('mysql:dbname=auth;host=127.0.0.1;charset=utf8mb4', 'authz', 'xP9tM715UK');
+  $auth = new \Delight\Auth\Auth($db1);
+$outp3 = "";
+if (($auth->isLoggedIn()) && ($auth->hasRole(\Delight\Auth\Role::ADMIN))) {
+  $outp3 = '{"AdminIsIn":true}';
+  $adminOk = true;
+} else {
+  $outp3 = '{"AdminIsIn":false}';
+  $adminOk = false;
 }
 
+if ($adminOk) {
+  // print_r($_POST['item']);
+  if (isset($_POST['item'])) {
+    $items = $_POST['item'];
+    // print_r($items);
+    // print_r($_FILES['file']);
+  }
+  if(isset($_FILES['file'])){
+      $errors= array();
+      $file_name = $_FILES['file']['name'];
+      $file_size =$_FILES['file']['size'];
+      $file_tmp =$_FILES['file']['tmp_name'];
+      $file_type=$_FILES['file']['type'];
+      $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+      $extensions = array("jpeg","jpg","png","pdf");
+      if(in_array($file_ext,$extensions )=== false){
+           $errors[]="File extension not allowed, please choose a JPEG or PNG file.";
+      }
+      if($file_size > 1097152){
+          $errors[]='File size cannot exceed 1 MB';
+      }
+      if(file_exists('../img/'.$file_name)) {
+        $errors[]='File '.$file_name.' already exists';
+      }
+      if(empty($errors)==true){
+          $currentDate = date('ymdhms');
+          $newFileName = $currentDate.$file_name;
+          move_uploaded_file($file_tmp,"../img/".$newFileName);
+          $rid = toDbase($items, $newFileName);
+
+          $items['image'] = '../img/'.$newFileName;
+          $outp2 = '{"message": "The record and the image updated successfully"}';
+          $outp1 = '{"updateitem":['.json_encode($items).']}';
+          $outp  = '{"info":['.$outp1.','.$outp2.','.$outp3.']}';
+
+          echo ($outp);
+      }else{
+          print_r($errors[0]);
+      }
+  }
+  else{
+    $newFileName = $items['image'];
+    toDbase($items, $newFileName);
+
+    $outp2 = '{"message": "The record updated with the current image successfully"}';
+    $outp1 = '{"updateitem":['.json_encode($items).']}';
+    $outp  = '{"info":['.$outp1.','.$outp2.','.$outp3.']}';
+
+    echo ($outp);
+  }
+
+} else {
+  $outp2 = '{"message": "null"}';
+  $outp1 = '{"updateitem":"null"}';
+  $outp  = '{"info":['.$outp1.','.$outp2.','.$outp3.']}';
+}
+
+//********************************************************************//
 function toDbase($items, $newFileName) {
   include('class/mysql_crud.php');
   $db = new Database();
--- php/upload.php
+++ php/upload.php
@@ -1,70 +1,90 @@
 <?php
-// print_r($_POST['item']);
-if (isset($_POST['item'])) {
-  $items = $_POST['item'];
-  // print_r($items);
-}
-if(!isset($_FILES['file'])){
-  $outp2 = '{"message": "No image selected"}';
-  $outp1 = '{"newitem": "null"}';
-  $outp  = '{"info":['.$outp1.','.$outp2.']}';
-  echo ($outp);
-} elseif (!isset($items['sname'])) {
-  $outp2 = '{"message": "Please fill დასახელება field"}';
-  $outp1 = '{"newitem": "null"}';
-  $outp  = '{"info":['.$outp1.','.$outp2.']}';
-  echo ($outp);
-  return;
-} elseif (!isset($items['sdescription'])) {
-  $outp2 = '{"message": "Please fill აღწერა field"}';
-  $outp1 = '{"newitem": "null"}';
-  $outp  = '{"info":['.$outp1.','.$outp2.']}';
-  echo ($outp);
-  return;
-} elseif (!isset($items['sprice'])) {
-  $outp2 = '{"message": "Please fill ფასი field"}';
-  $outp1 = '{"newitem": "null"}';
-  $outp  = '{"info":['.$outp1.','.$outp2.']}';
-  echo ($outp);
-  return;
+
+require '../vendor/autoload.php';
+  $db1 = new \PDO('mysql:dbname=auth;host=127.0.0.1;charset=utf8mb4', 'authz', 'xP9tM715UK');
+  $auth = new \Delight\Auth\Auth($db1);
+$outp3 = "";
+if (($auth->isLoggedIn()) && ($auth->hasRole(\Delight\Auth\Role::ADMIN))) {
+  $outp3 = '{"AdminIsIn":true}';
+  $adminOk = true;
+} else {
+  $outp3 = '{"AdminIsIn":false}';
+  $adminOk = false;
 }
 
-if(isset($_FILES['file'])){
-    $errors= array();
-    $file_name = $_FILES['file']['name'];
-    $file_size =$_FILES['file']['size'];
-    $file_tmp =$_FILES['file']['tmp_name'];
-    $file_type=$_FILES['file']['type'];
-    $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
-    $extensions = array("jpeg","jpg","png","pdf");
-    if(in_array($file_ext,$extensions )=== false){
-         $errors[]="File extension not allowed, please choose a JPEG or PNG file.";
-    }
-    if($file_size > 1097152){
-        $errors[]='File size cannot exceed 1 MB';
-    }
-    if(file_exists('../img/'.$file_name)) {
-      $errors[]='File '.$file_name.' already exists';
-    }
-    if(empty($errors)==true){
-        $currentDate = date('ymdhms');
-        $newFileName = $currentDate.$file_name;
-        move_uploaded_file($file_tmp,"../img/".$newFileName);
-        $sid = toDbase($items, $newFileName);
+if ($adminOk) {
+  // print_r($_POST['item']);
+  if (isset($_POST['item'])) {
+    $items = $_POST['item'];
+    // print_r($items);
+  }
+  if(!isset($_FILES['file'])){
+    $outp2 = '{"message": "No image selected"}';
+    $outp1 = '{"newitem": "null"}';
+    $outp  = '{"info":['.$outp1.','.$outp2.']}';
+    echo ($outp);
+  } elseif (!isset($items['sname'])) {
+    $outp2 = '{"message": "Please fill დასახელება field"}';
+    $outp1 = '{"newitem": "null"}';
+    $outp  = '{"info":['.$outp1.','.$outp2.']}';
+    echo ($outp);
+    return;
+  } elseif (!isset($items['sdescription'])) {
+    $outp2 = '{"message": "Please fill აღწერა field"}';
+    $outp1 = '{"newitem": "null"}';
+    $outp  = '{"info":['.$outp1.','.$outp2.']}';
+    echo ($outp);
+    return;
+  } elseif (!isset($items['sprice'])) {
+    $outp2 = '{"message": "Please fill ფასი field"}';
+    $outp1 = '{"newitem": "null"}';
+    $outp  = '{"info":['.$outp1.','.$outp2.']}';
+    echo ($outp);
+    return;
+  }
 
-        if (isset($sid)) {
-          $items['id'] = $sid[0];
-        }
-        $items['simage'] = '../img/'.$newFileName;
-        $outp2 = '{"message": "The record and the image uploaded successfully"}';
-        $outp1 = '{"newitem":['.json_encode($items).']}';
-        $outp  = '{"info":['.$outp1.','.$outp2.']}';
-        echo ($outp);
-    }else{
-        print_r($errors[0]);
-    }
+  if(isset($_FILES['file'])){
+      $errors= array();
+      $file_name = $_FILES['file']['name'];
+      $file_size =$_FILES['file']['size'];
+      $file_tmp =$_FILES['file']['tmp_name'];
+      $file_type=$_FILES['file']['type'];
+      $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+      $extensions = array("jpeg","jpg","png","pdf");
+      if(in_array($file_ext,$extensions )=== false){
+           $errors[]="File extension not allowed, please choose a JPEG or PNG file.";
+      }
+      if($file_size > 1097152){
+          $errors[]='File size cannot exceed 1 MB';
+      }
+      if(file_exists('../img/'.$file_name)) {
+        $errors[]='File '.$file_name.' already exists';
+      }
+      if(empty($errors)==true){
+          $currentDate = date('ymdhms');
+          $newFileName = $currentDate.$file_name;
+          move_uploaded_file($file_tmp,"../img/".$newFileName);
+          $sid = toDbase($items, $newFileName);
+
+          if (isset($sid)) {
+            $items['id'] = $sid[0];
+          }
+          $items['simage'] = '../img/'.$newFileName;
+          $outp1 = '{"newitem":['.json_encode($items).']}';
+          $outp2 = '{"message": "The record and the image uploaded successfully"}';
+          $outp  = '{"info":['.$outp1.','.$outp2.','.$outp3.']}';
+          echo ($outp);
+      }else{
+          print_r($errors[0]);
+      }
+  }
+} else {
+  $outp1 = '{"newitem":"null"}';
+  $outp2 = '{"message": "null"}';
+  $outp  = '{"info":['.$outp1.','.$outp2.','.$outp3.']}';
 }
 
+//*******************************************************//
 function toDbase($items, $newFileName) {
   include('class/mysql_crud.php');
   $db = new Database();
